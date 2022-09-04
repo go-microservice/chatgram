6e2260f11abca7c8a3669350202fcecc8a617d5b
qloog <quanlongwang@gmail.com>
2022-09-04 21:11:29 +0800

    chore: add check for params

--- internal/service/relationservice_grpc.go
+++ internal/service/relationservice_grpc.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 
+	"github.com/go-eagle/eagle/pkg/errcode"
+
 	relationV1 "github.com/go-microservice/relation-service/api/relation/v1"
 	userv1 "github.com/go-microservice/user-service/api/user/v1"
 	"github.com/jinzhu/copier"
@@ -29,11 +31,28 @@ func NewRelationServiceServer(repo relationV1.RelationServiceClient, userRepo us
 }
 
 func (s *RelationServiceServer) Follow(ctx context.Context, req *pb.FollowRequest) (*pb.FollowReply, error) {
+	// check user is exist
+	user, err := s.userRPC.GetUser(ctx, &userv1.GetUserRequest{Id: req.GetUserId()})
+	if err != nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	if user == nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	user, err = s.userRPC.GetUser(ctx, &userv1.GetUserRequest{Id: req.GetFollowedUid()})
+	if err != nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	if user == nil {
+		return nil, errcode.ErrInvalidParam
+	}
+
+	// exec follow logic
 	in := &relationV1.FollowRequest{
 		UserId:      req.UserId,
 		FollowedUid: req.FollowedUid,
 	}
-	_, err := s.relationRPC.Follow(ctx, in)
+	_, err = s.relationRPC.Follow(ctx, in)
 	if err != nil {
 		return nil, err
 	}
@@ -41,11 +60,27 @@ func (s *RelationServiceServer) Follow(ctx context.Context, req *pb.FollowReques
 }
 
 func (s *RelationServiceServer) Unfollow(ctx context.Context, req *pb.UnfollowRequest) (*pb.UnfollowReply, error) {
+	// check user is exist
+	user, err := s.userRPC.GetUser(ctx, &userv1.GetUserRequest{Id: req.GetUserId()})
+	if err != nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	if user == nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	user, err = s.userRPC.GetUser(ctx, &userv1.GetUserRequest{Id: req.GetFollowedUid()})
+	if err != nil {
+		return nil, errcode.ErrInvalidParam
+	}
+	if user == nil {
+		return nil, errcode.ErrInvalidParam
+	}
+
 	in := &relationV1.UnfollowRequest{
 		UserId:      req.UserId,
 		FollowedUid: req.FollowedUid,
 	}
-	_, err := s.relationRPC.Unfollow(ctx, in)
+	_, err = s.relationRPC.Unfollow(ctx, in)
 	if err != nil {
 		return nil, err
 	}
@@ -53,8 +88,14 @@ func (s *RelationServiceServer) Unfollow(ctx context.Context, req *pb.UnfollowRe
 }
 
 func (s *RelationServiceServer) GetFollowingUserList(ctx context.Context, req *pb.GetFollowingUserListRequest) (*pb.GetFollowingUserListReply, error) {
+	if req.GetId() == "" {
+		return nil, errcode.ErrInvalidParam
+	}
 	// get relation data, support pagination
 	limit := cast.ToInt32(req.GetLimit())
+	if limit == 0 {
+		limit = 10
+	}
 
 	in := &relationV1.FollowingListRequest{
 		UserId: cast.ToInt64(req.GetId()),
@@ -105,8 +146,14 @@ func (s *RelationServiceServer) GetFollowingUserList(ctx context.Context, req *p
 	}, nil
 }
 func (s *RelationServiceServer) GetFollowerUserList(ctx context.Context, req *pb.GetFollowerUserListRequest) (*pb.GetFollowerUserListReply, error) {
+	if req.GetUserId() == "" {
+		return nil, errcode.ErrInvalidParam
+	}
 	// get relation data, support pagination
 	limit := cast.ToInt32(req.GetLimit())
+	if limit == 0 {
+		limit = 10
+	}
 	in := &relationV1.FollowerListRequest{
 		UserId: cast.ToInt64(req.GetUserId()),
 		LastId: cast.ToInt64(req.GetLastId()),
